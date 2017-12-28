e0e9663ba361c31e772abac368734859deb365f8
Benjamin Daniel <benjamindaniel706@gmail.com>
2017-12-28 18:56:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,21 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $(this).addClass("tasty");
+  })
+}
+function pressIt() {
+  $("input#typing").on("keydown", function(key) {
+    
+  })
+}
